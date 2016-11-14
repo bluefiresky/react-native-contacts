dad3dbba15d7637119946c012aa99a9b3403650d
bluefiresky <616125411@qq.com>
2016-11-14 19:01:26 +0800

    init the project

---
+++ README.md
@@ -0,0 +1,212 @@
+# React Native Contacts
+Rx support with [react-native-contacts-rx](https://github.com/JeanLebrument/react-native-contacts-rx)
+
+## Status
+* Preliminary iOS and Android support
+* API subject to revision, changelog in release notes  
+
+| Feature | iOS | Android |
+| ------- | --- | ------- |
+| `getAll`  | ✔   | ✔ |
+| `addContact` | ✔ | ✔ |
+| `updateContact` | ✔ | ✔ |
+| `deleteContact` | ✔ | 😞 |
+| get with options | 😞 | 😞 |
+| groups  | 😞 | 😞 |
+
+
+
+## API
+`getAll` (callback) - returns *all* contacts as an array of objects  
+`addContact` (contact, callback) - adds a contact to the AddressBook.  
+`updateContact` (contact, callback) - where contact is an object with a valid recordID  
+`deleteContact` (contact, callback) - where contact is an object with a valid recordID  
+`checkPermission` (callback) - checks permission to access Contacts  
+`requestPermission` (callback) - request permission to access Contacts
+
+## Usage
+`getAll` is a database intensive process, and can take a long time to complete depending on the size of the contacts list. Because of this, it is recommended you access the `getAll` method before it is needed, and cache the results for future use.
+
+Also there is a lot of room for performance enhancements in both iOS and android. PR's welcome!
+
+```js
+var Contacts = require('react-native-contacts')
+
+Contacts.getAll((err, contacts) => {
+  if(err && err.type === 'permissionDenied'){
+    // x.x
+  } else {
+    console.log(contacts)
+  }
+})
+```
+
+## Example Contact Record
+```js
+{
+  recordID: 1,
+  familyName: "Jung",
+  givenName: "Carl",
+  middleName: "",
+  emailAddresses: [{
+    label: "work",
+    email: "carl-jung@example.com",
+  }],
+  phoneNumbers: [{
+    label: "mobile",
+    number: "(555) 555-5555",
+  }],
+  thumbnailPath: "",
+}
+```
+**NOTE**
+* on Android the entire display name is passed in the `givenName` field. `middleName` and `familyName` will be `""`.
+
+## Adding Contacts
+Currently all fields from the contact record except for thumbnailPath are supported for writing
+```js
+var newPerson = {
+  familyName: "Nietzsche",
+  givenName: "Friedrich",
+  emailAddresses: [{
+    label: "work",
+    email: "mrniet@example.com",
+  }],
+}
+
+Contacts.addContact(newPerson, (err) => { /*...*/ })
+```
+
+## Updating and Deleting Contacts
+```js
+//contrived example
+Contacts.getAll( (err, contacts) => {
+  //update the first record
+  let someRecord = contacts[0]
+  someRecord.emailAddresses.push({
+    label: "junk",
+    email: "mrniet+junkmail@test.com",
+  })
+  Contacts.updateContact(someRecord, (err) => { /*...*/ })
+
+  //delete the second record
+  Contacts.deleteContact(contacts[1], (err) => { /*...*/ })
+})
+```
+Update and delete reference contacts by their recordID (as returned by the OS in getContacts). Apple does not guarantee the recordID will not change, e.g. it may be reassigned during a phone migration. Consequently you should always grab a fresh contact list with `getContacts` before performing update and delete operations.
+
+You can also delete a record using only it's recordID like follows: `Contacts.deleteContact({recordID: 1}, (err) => {})}`
+
+## Displaying Thumbnails
+
+The thumbnailPath is the direct URI for the temp location of the contact's cropped thumbnail image.
+
+```js
+<Image source={{uri: contact.thumbnailPath}} />
+```
+
+## Getting started
+run `npm install react-native-contacts`
+
+### iOS
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. add `./node_modules/react-native-contacts/ios/RCTContacts.xcodeproj`
+3. In the XCode project navigator, select your project, select the `Build Phases` tab and in the `Link Binary With Libraries` section add **libRCTContacts.a**
+
+#### Permissions
+As of Xcode 8 and React Native 0.33 it is now **necessary to add kit specific "permission" keys** to your Xcode `Info.plist` file, in order to make `requestPermission` work. Otherwise your app crashes when requesting the specific permission. I discovered this after days of frustration.
+
+Open Xcode > Info.plist > Add a key (starting with "Privacy - ...") with your kit specific permission. The value for the key is optional.
+
+You have to add the key "Privacy - Contacts Usage Description".
+
+<img width="338" alt="screen shot 2016-09-21 at 13 13 21" src="https://cloud.githubusercontent.com/assets/5707542/18704973/3cde3b44-7ffd-11e6-918b-63888e33f983.png">
+
+### Android
+* In `android/settings.gradle`
+```gradle
+...
+include ':react-native-contacts'
+project(':react-native-contacts').projectDir = new File(settingsDir, '../node_modules/react-native-contacts/android')
+```
+
+* In `android/app/build.gradle`
+```gradle
+...
+dependencies {
+    ...
+    compile project(':react-native-contacts')
+}
+```
+
+* register module (in android/app/src/main/java/[your-app-namespace]/MainActivity.java)
+```java
+	...
+
+	import com.rt2zz.reactnativecontacts.ReactNativeContacts; 	// <--- import module!
+
+	public class MainActivity extends ReactActivity {
+		...
+
+	   	/**
+	   	* A list of packages used by the app. If the app uses additional views
+	   	* or modules besides the default ones, add more packages here.
+	   	*/
+	    @Override
+	    protected List<ReactPackage> getPackages() {
+	      return Arrays.<ReactPackage>asList(
+	        new MainReactPackage(),
+	        new ReactNativeContacts() 	// <--- and add package
+	      );
+	    }
+
+    	...
+    }
+```
+If you are using an older version of MainActivity (i.e. `public class MainActivity extends Activity`) please see the [old instructions](https://github.com/rt2zz/react-native-contacts/tree/1ce4b876a416bc2ca3c53e7d7e0296f7fcb7ce40#android)
+
+* add Contacts permission (in android/app/src/main/AndroidManifest.xml)
+(only add the permissions you need)
+```xml
+...
+  <uses-permission android:name="android.permission.READ_CONTACTS" />
+  <uses-permission android:name="android.permission.WRITE_CONTACTS" />
+  <uses-permission android:name="android.permission.READ_PROFILE" />
+...
+```
+
+## Permissions Methods (optional)
+`checkPermission` (callback) - checks permission to access Contacts.  
+`requestPermission` (callback) - request permission to access Contacts.  
+
+Usage as follows:
+```js
+Contacts.checkPermission( (err, permission) => {
+  // Contacts.PERMISSION_AUTHORIZED || Contacts.PERMISSION_UNDEFINED || Contacts.PERMISSION_DENIED
+  if(permission === 'undefined'){
+    Contacts.requestPermission( (err, permission) => {
+      // ...
+    })
+  }
+  if(permission === 'authorized'){
+    // yay!
+  }
+  if(permission === 'denied'){
+    // x.x
+  }
+})
+```
+
+These methods do **not** re-request permission if permission has already been granted or denied. This is a limitation in iOS, the best you can do is prompt the user with instructions for how to enable contacts from the phone settings page `Settings > [app name] > contacts`.
+
+On android permission request is done upon install so this function will only show if the  permission has been granted.
+
+## Todo
+- [ ] android feature parity
+- [ ] migrate iOS from AddressBook to Contacts
+- [ ] implement `get` with options
+- [ ] groups support
+
+## LICENSE
+
+[MIT License](LICENSE)
---
+++ android/src/main/java/com/rt2zz/reactnativecontacts/ContactsProvider.java
@@ -0,0 +1,306 @@
+package com.rt2zz.reactnativecontacts;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableArray;
+import com.facebook.react.bridge.WritableMap;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.provider.ContactsContract.CommonDataKinds.Contactables;
+import static android.provider.ContactsContract.CommonDataKinds.Email;
+import static android.provider.ContactsContract.CommonDataKinds.Phone;
+import static android.provider.ContactsContract.CommonDataKinds.StructuredName;
+
+public class ContactsProvider {
+    public static final int ID_FOR_PROFILE_CONTACT = -1;
+
+    private static final List<String> JUST_ME_PROJECTION = new ArrayList<String>() {{
+        add(ContactsContract.Contacts.Data.MIMETYPE);
+        add(ContactsContract.Profile.DISPLAY_NAME);
+        add(Contactables.PHOTO_URI);
+        add(StructuredName.DISPLAY_NAME);
+        add(StructuredName.GIVEN_NAME);
+        add(StructuredName.MIDDLE_NAME);
+        add(StructuredName.FAMILY_NAME);
+        add(Phone.NUMBER);
+        add(Phone.TYPE);
+        add(Phone.LABEL);
+        add(Email.DATA);
+        add(Email.ADDRESS);
+        add(Email.TYPE);
+        add(Email.LABEL);
+    }};
+
+    private static final List<String> FULL_PROJECTION = new ArrayList<String>() {{
+        add(ContactsContract.Data.CONTACT_ID);
+        add(ContactsContract.RawContacts.SOURCE_ID);
+        addAll(JUST_ME_PROJECTION);
+    }};
+
+    private final ContentResolver contentResolver;
+    private final Context context;
+
+    public ContactsProvider(ContentResolver contentResolver, Context context) {
+        this.contentResolver = contentResolver;
+        this.context = context;
+    }
+
+    public WritableArray getContacts() {
+        Map<String, Contact> justMe;
+        {
+            Cursor cursor = contentResolver.query(
+                    Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI, ContactsContract.Contacts.Data.CONTENT_DIRECTORY),
+                    JUST_ME_PROJECTION.toArray(new String[JUST_ME_PROJECTION.size()]),
+                    null,
+                    null,
+                    null
+            );
+
+            try {
+                justMe = loadContactsFrom(cursor);
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+
+        Map<String, Contact> everyoneElse;
+        {
+            Cursor cursor = contentResolver.query(
+                    ContactsContract.Data.CONTENT_URI,
+                    FULL_PROJECTION.toArray(new String[FULL_PROJECTION.size()]),
+                    ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=? OR " + ContactsContract.Data.MIMETYPE + "=?",
+                    new String[]{Email.CONTENT_ITEM_TYPE, Phone.CONTENT_ITEM_TYPE, StructuredName.CONTENT_ITEM_TYPE},
+                    null
+            );
+
+            try {
+                everyoneElse = loadContactsFrom(cursor);
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+
+        WritableArray contacts = Arguments.createArray();
+        for (Contact contact : justMe.values()) {
+            contacts.pushMap(contact.toMap());
+        }
+        for (Contact contact : everyoneElse.values()) {
+            contacts.pushMap(contact.toMap());
+        }
+
+        return contacts;
+    }
+
+    @NonNull
+    private Map<String, Contact> loadContactsFrom(Cursor cursor) {
+
+        Map<String, Contact> map = new LinkedHashMap<>();
+
+        while (cursor != null && cursor.moveToNext()) {
+
+            int columnIndex = cursor.getColumnIndex(ContactsContract.Data.CONTACT_ID);
+            String contactId;
+            if (columnIndex != -1) {
+                contactId = String.valueOf(cursor.getInt(columnIndex));
+            } else {
+                contactId = String.valueOf(ID_FOR_PROFILE_CONTACT);//no contact id for 'ME' user
+            }
+
+            columnIndex = cursor.getColumnIndex(ContactsContract.RawContacts.SOURCE_ID);
+            if (columnIndex != -1) {
+                String uid = cursor.getString(columnIndex);
+                if (uid != null) {
+                    contactId = uid;
+                }
+            }
+
+            if (!map.containsKey(contactId)) {
+                map.put(contactId, new Contact(contactId));
+            }
+
+            Contact contact = map.get(contactId);
+
+            String mimeType = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.MIMETYPE));
+
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            if (!TextUtils.isEmpty(name) && TextUtils.isEmpty(contact.displayName)) {
+                contact.displayName = name;
+            }
+
+            String rawPhotoURI = cursor.getString(cursor.getColumnIndex(Contactables.PHOTO_URI));
+            if (!TextUtils.isEmpty(rawPhotoURI)) {
+                contact.photoUri = getPhotoURIFromContactURI(rawPhotoURI, contactId);
+            }
+
+            if (mimeType.equals(StructuredName.CONTENT_ITEM_TYPE)) {
+                contact.givenName = cursor.getString(cursor.getColumnIndex(StructuredName.GIVEN_NAME));
+                contact.middleName = cursor.getString(cursor.getColumnIndex(StructuredName.MIDDLE_NAME));
+                contact.familyName = cursor.getString(cursor.getColumnIndex(StructuredName.FAMILY_NAME));
+            } else if (mimeType.equals(Phone.CONTENT_ITEM_TYPE)) {
+                String phoneNumber = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
+                int type = cursor.getInt(cursor.getColumnIndex(Phone.TYPE));
+
+                if (!TextUtils.isEmpty(phoneNumber)) {
+                    String label;
+                    switch (type) {
+                        case Phone.TYPE_HOME:
+                            label = "home";
+                            break;
+                        case Phone.TYPE_WORK:
+                            label = "work";
+                            break;
+                        case Phone.TYPE_MOBILE:
+                            label = "mobile";
+                            break;
+                        default:
+                            label = "other";
+                    }
+                    contact.phones.add(new Contact.Item(label, phoneNumber));
+                }
+            } else if (mimeType.equals(Email.CONTENT_ITEM_TYPE)) {
+                String email = cursor.getString(cursor.getColumnIndex(Email.ADDRESS));
+                int type = cursor.getInt(cursor.getColumnIndex(Email.TYPE));
+
+                if (!TextUtils.isEmpty(email)) {
+                    String label;
+                    switch (type) {
+                        case Email.TYPE_HOME:
+                            label = "home";
+                            break;
+                        case Email.TYPE_WORK:
+                            label = "work";
+                            break;
+                        case Email.TYPE_MOBILE:
+                            label = "mobile";
+                            break;
+                        case Email.TYPE_CUSTOM:
+                            if (cursor.getString(cursor.getColumnIndex(Email.LABEL)) != null) {
+                                label = cursor.getString(cursor.getColumnIndex(Email.LABEL)).toLowerCase();
+                            } else {
+                                label = "";
+                            }
+                            break;
+                        default:
+                            label = "other";
+                    }
+                    contact.emails.add(new Contact.Item(label, email));
+                }
+            }
+        }
+
+        return map;
+    }
+
+    private String getPhotoURIFromContactURI(String contactURIString, String contactId) {
+        Uri contactURI = Uri.parse(contactURIString);
+
+        try {
+            InputStream photoStream = contentResolver.openInputStream(contactURI);
+
+            if (photoStream == null)
+                return "";
+
+            try {
+                BufferedInputStream in = new BufferedInputStream(photoStream);
+                File outputDir = context.getCacheDir(); // context being the Activity pointer
+                File outputFile = File.createTempFile("contact" + contactId, ".jpg", outputDir);
+                FileOutputStream output = new FileOutputStream(outputFile);
+
+                try {
+                    int count;
+                    byte[] buffer = new byte[4098];
+
+                    while ((count = in.read(buffer)) > 0) {
+                        output.write(buffer, 0, count);
+                    }
+                } catch (IOException e) {
+                    output.close();
+                }
+
+                in.close();
+
+                return "file://" + outputFile.getAbsolutePath();
+            } finally {
+                photoStream.close();
+            }
+        } catch (IOException e) {
+            Log.w("RNContacts", "Failed to get photo uri", e);
+            return "";
+        }
+    }
+
+    private static class Contact {
+        private String contactId;
+        private String displayName;
+        private String givenName = "";
+        private String middleName = "";
+        private String familyName = "";
+        private String photoUri;
+        private List<Item> emails = new ArrayList<>();
+        private List<Item> phones = new ArrayList<>();
+
+        public Contact(String contactId) {
+            this.contactId = contactId;
+        }
+
+        public WritableMap toMap() {
+            WritableMap contact = Arguments.createMap();
+            contact.putString("recordID", contactId);
+            contact.putString("givenName", TextUtils.isEmpty(givenName) ? displayName : givenName);
+            contact.putString("middleName", middleName);
+            contact.putString("familyName", familyName);
+            contact.putString("thumbnailPath", photoUri == null ? "" : photoUri);
+
+            WritableArray phoneNumbers = Arguments.createArray();
+            for (Item item : phones) {
+                WritableMap map = Arguments.createMap();
+                map.putString("number", item.value);
+                map.putString("label", item.label);
+                phoneNumbers.pushMap(map);
+            }
+            contact.putArray("phoneNumbers", phoneNumbers);
+
+            WritableArray emailAddresses = Arguments.createArray();
+            for (Item item : emails) {
+                WritableMap map = Arguments.createMap();
+                map.putString("email", item.value);
+                map.putString("label", item.label);
+                emailAddresses.pushMap(map);
+            }
+            contact.putArray("emailAddresses", emailAddresses);
+
+            return contact;
+        }
+
+        public static class Item {
+            public String label;
+            public String value;
+
+            public Item(String label, String value) {
+                this.label = label;
+                this.value = value;
+            }
+        }
+    }
+}
